9e871c859cf7a9b4a55de3027e68f21c1ef33078
guanglun <272073020@qq.com>
2021-03-26 11:32:20 +0800

    update

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,8 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as sidebar from './test';
+import * as path from 'path';
+import * as fs from 'fs';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -32,7 +34,41 @@ export function activate(context: vscode.ExtensionContext) {
 		sidebar_test.refresh2();
         vscode.window.showInformationMessage(args);
     });
+
+
+
+
+	context.subscriptions.push(vscode.commands.registerCommand('hello-code.openWebview', function (uri) {
+		// 建立webview
+		const panel = vscode.window.createWebviewPanel(
+			'testWebview', // viewType
+			"WebView演示", // 视图标题
+			vscode.ViewColumn.One, // 显示在编辑器的哪一个部位
+			{
+				enableScripts: true, // 启用JS，默认禁用
+				retainContextWhenHidden: true, // webview被隐藏时保持状态，避免被重置
+			}
+		);
+		panel.webview.html = getWebViewContent(context,'src/view/index.html');
+		}));
+}
+
+/**
+ * 从某个HTML文件读取能被Webview加载的HTML内容
+ * @param {*} context 上下文
+ * @param {*} templatePath 相对于插件根目录的html文件相对路径
+ */
+ function getWebViewContent(context: vscode.ExtensionContext, templatePath: string) {
+    const resourcePath = path.join(context.extensionPath, templatePath);
+    const dirPath = path.dirname(resourcePath);
+    let html = fs.readFileSync(resourcePath, 'utf-8');
+    // vscode不支持直接加载本地资源，须要替换成其专有路径格式，这里只是简单的将样式和JS的路径替换
+    html = html.replace(/(<link.+?href="|<script.+?src="|<img.+?src=")(.+?)"/g, (m, $1, $2) => {
+        return $1 + vscode.Uri.file(path.resolve(dirPath, $2)).with({ scheme: 'vscode-resource' }).toString() + '"';
+    });
+    return html;
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
